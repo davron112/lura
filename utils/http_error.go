8fb58dadcb3dcd506bb8ec07b5c47f7cb8222f7b
Achilov Davronbek <achilov21@yandex.com>
2024-03-04 22:18:23 +0500

    Update http_error.go
    Signed-off-by: Achilov Davronbek <achilov21@yandex.com>

--- utils/http_error.go
+++ utils/http_error.go
@@ -1,6 +1,11 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
 
 type HTTPError struct {
 	StatusCode int
@@ -14,3 +19,17 @@ func (e *HTTPError) Error() string {
 func NewHTTPError(statusCode int, message string) *HTTPError {
 	return &HTTPError{StatusCode: statusCode, Message: message}
 }
+
+
+func JsonHttpResponse(w http.ResponseWriter, statusCode int, data map[string]string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		// Log the error and return a generic error message if JSON marshaling fails
+		fmt.Fprintf(w, `{"error": "Internal server error"}`)
+		return
+	}
+	w.Write(jsonData)
+}
+
